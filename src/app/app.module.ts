3369b64485a688c9c5d4167ddac9dec90562b907
LukasKlammer <klammer.lukas@hotmail.com>
2022-06-03 08:42:40 +0200

    routings, generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { IntroComponent } from './intro/intro.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ImprintComponent,
+    DataProtectionComponent,
+    IntroComponent,
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule,

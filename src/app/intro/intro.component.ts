f837ba16da5f65b1e7c68f6627e50ba0986a3be1
LukasKlammer <klammer.lukas@hotmail.com>
2022-06-03 12:32:47 +0200

    intro section

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroComponent implements OnInit {
 
+  images = ['coding.jpg', 'uni_innsbruck.jpg', 'team.jpg'];
+  headlines = ['love coding','Graduated at the university of Innsbruck',  'good work is team work'];
+  currentImage = 0;
+  showImage = true;
+
   constructor() { }
 
   ngOnInit(): void {

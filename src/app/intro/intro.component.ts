04a78ee081cfadc63ea50b3cc532d3c7a5563de0
unknown <klammer.lukas@hotmail.com>
2022-06-20 12:16:48 +0200

    responsive intro component

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
@@ -8,13 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class IntroComponent implements OnInit {
 
   images = ['coding.jpg', 'uni_innsbruck.jpg', 'team.jpg'];
-  headlines = ['love coding','Graduated at the university of Innsbruck',  'good work is team work'];
+  headlines = ['love coding', 'Graduated at the university of Innsbruck', 'good work is team work'];
   currentImage = 0;
   showImage = true;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }

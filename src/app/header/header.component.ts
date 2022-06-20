b11549f45559ecd2d468488ab843259b06c3ff74
unknown <klammer.lukas@hotmail.com>
2022-06-20 14:16:58 +0200

    show / hide menu in base of screen width

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isMobile:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener("window:resize", []) updateDays() {
+    if (window.innerWidth >= 700) {
+      this.isMobile = false;
+    } else if (window.innerWidth < 700) {
+      this.isMobile = true;
+    }
+  }
 }

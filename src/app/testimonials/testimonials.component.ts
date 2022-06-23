4765df438b41ff23cdcf4d9f86c1dc066797cdb3
unknown <klammer.lukas@hotmail.com>
2022-06-23 11:21:28 +0200

    add about me text and testimonial

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -22,9 +22,9 @@ export class TestimonialsComponent implements OnInit {
     },
     {
       img: 'assets/img/team.jpg',
-      statement: '"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam velit, architecto aliquid, blanditiis voluptatem ipsa unde corrupti natus enim quia quibusdam dignissimos quo. Beatae veniam, pariatur perferendis deserunt accusamus distinctio?"',
-      name: 'Stefan Keim',
-      role: 'school leader'
+      statement: '"Lukas has a high degree of initiative and a willingness to do more than what is necessary. This simplifies cooperation and leads to the speedy and successful completion of various projects."',
+      name: 'Lukas Pfendt',
+      role: 'IT systems administrator'
     },
   ];
 

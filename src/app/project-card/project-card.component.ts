44c2aeca48b3d3f0bc5c2c201b5dcdd3a2a9591f
unknown <klammer.lukas@hotmail.com>
2022-07-07 10:55:04 +0200

    hover animation project card, add testimonials, css style about me image

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -5,15 +5,23 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
-export class ProjectCardComponent implements OnInit {
+export class ProjectCardComponent {
 
   @Input() img:string = 'assets/img/logo_white.png';
   @Input() headline:string = 'skill headline';
   @Input() description:string = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo reprehenderit tempora atque debitis, quas saepe facilis officiis ex! Consectetur saepe aperiam ipsum ab eos repellendus eum iste nemo quasi pariatur.';
 
+
+  isHovered = false;
+
   constructor() { }
 
-  ngOnInit(): void {
+  public hoverEffectOn() {
+    this.isHovered = true;
+  }
+
+  public hoverEffectOff() {
+    this.isHovered = false;
   }
 
 }
--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -9,16 +9,22 @@ export class TestimonialsComponent {
 
   testimonials = [
     {
-      img: 'assets/img/digital-learning.jpg',
+      img: 'assets/img/digital_learning.jpg',
       statement: '"Lukas is a very skilled and hard working teacher, his main skills are to be found in digital learning and teaching. He was an important force in building up a progressive and well working system of digital learning in the secondary-school “Dr. Josef Röd” in Bruneck, Italy."',
       name: 'Stefan Keim',
-      role: 'headmaster'
+      role: 'Headmaster'
     },
     {
       img: 'assets/img/team.jpg',
       statement: '"Lukas has a high degree of initiative and a willingness to do more than what is necessary. This simplifies cooperation and leads to the speedy and successful completion of various projects."',
       name: 'Lukas Pfendt',
       role: 'IT systems administrator'
+    },
+    {
+      img: 'assets/img/focus_work.jpg',
+      statement: '"All I can say that, Lukas works works purposefully, deliberately and with focus, always looking for meaning in what he does."',
+      name: 'Claudia Rieder',
+      role: 'Head of BYOD department'
     },
   ];
 
